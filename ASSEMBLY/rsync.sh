c50ae020808d52a33be457cc2d2a9eff16262bf9
QRunMiao <102786938+QRunMiao@users.noreply.github.com>
2023-08-21 18:55:27 +0800

    Add files via upload

--- ASSEMBLY/rsync.sh
+++ ASSEMBLY/rsync.sh
@@ -1,1 +1,75 @@
+#!/usr/bin/env bash
 
+BASH_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd "${BASH_DIR}"
+
+#----------------------------#
+# Colors in term
+#----------------------------#
+GREEN=
+RED=
+NC=
+if tty -s < /dev/fd/1 2> /dev/null; then
+    GREEN='\033[0;32m'
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+fi
+
+log_warn () {
+    echo >&2 -e "${RED}==> $@ <==${NC}"
+}
+
+log_info () {
+    echo >&2 -e "${GREEN}==> $@${NC}"
+}
+
+log_debug () {
+    echo >&2 -e "==> $@"
+}
+
+export -f log_warn
+export -f log_info
+export -f log_debug
+
+#----------------------------#
+# helper functions
+#----------------------------#
+set +e
+
+# set stacksize to unlimited
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    ulimit -s unlimited
+fi
+
+signaled () {
+    log_warn Interrupted
+    exit 1
+}
+trap signaled TERM QUIT INT
+
+readlinkf () {
+    perl -MCwd -l -e 'print Cwd::abs_path shift' "$1";
+}
+
+export -f readlinkf
+
+#----------------------------#
+# Run
+#----------------------------#
+log_warn rsync.sh
+
+touch check.lst
+
+cat url.tsv |
+    tsv-join -f check.lst -k 1 -e |
+    parallel --colsep '\t' --no-run-if-empty --linebuffer -k -j 4 '
+        echo >&2
+        log_info "{3}\t{1}"
+        mkdir -p "{3}/{1}"
+        rsync -avP --no-links {2}/ {3}/{1}/ --exclude="assembly_status.txt"
+    '
+
+log_info Done.
+
+exit 0
